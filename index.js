46a1ae1d1a6242287c87077a284151172f052953
Ankit kumar <ankitkumarakb002@gmail.com>
2024-06-20 07:58:09 +0530

    Profilesharemodal

---
+++ index.js
@@ -0,0 +1,17 @@
+const modal=document.querySelector(".modal");
+const overlay=document.querySelector(".overlay");
+// const follow=document.querySelector(".btn");
+
+
+const openmodal=()=>{
+    console.log("modal is open");
+    modal.classList.add("active");
+    overlay.classList.add("overlayactive");
+};
+const closemodal=()=>{
+    console.log("modal is close");
+    modal.classList.remove("active");
+    overlay.classList.remove("overlayactive");
+    
+   
+};
